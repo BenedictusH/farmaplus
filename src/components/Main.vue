a9b5781b3f621444c64b1767606c43c6d53481db
Gabriel Fabiano <gbrfabiano@gmail.com>
2021-07-05 23:48:22 +0700

    layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,42 +1,111 @@
 <template>
-  <v-container class="wrapper">
+  <div class="wrapper">
     <v-parallax
       src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
       class="header"
+      style="height: 200px;"
     ></v-parallax>
-    <div class="main">
-      <div class="left">
-        <div class=" search"><v-autocomplete label="Tempat"></v-autocomplete></div>
-        <div class=" search">
-          <v-autocomplete label="Nama Obat" v-model="selectedObat" :items="obat"></v-autocomplete>
-        </div>
-      </div>
-      <div class="right">
-        <v-col v-for="apotek in dummyData" :key="apotek" cols="12">
+    <v-row class="px-16 pt-10" no-gutters>
+      <v-col cols="8" class="px-5">
+        <v-text-field
+            filled rounded dense
+            label="Search"
+            append-icon="mdi-magnify"
+            v-model="searchbar"
+          ></v-text-field>
+      </v-col>
+      <v-col cols="4" class="px-5">
+        <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="dates"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-combobox
+            v-model="dates"
+            multiple
+            chips
+            small-chips
+            filled rounded
+            label="Pilih Tanggal"
+            prepend-inner-icon="mdi-calendar"
+            clearable
+            deletable-chips
+            v-bind="attrs"
+            v-on="on"
+          ></v-combobox>
+        </template>
+        <v-date-picker
+          v-model="dates"
+          multiple
+          no-title
+          scrollable
+        >
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            color="primary"
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            text
+            color="primary"
+            @click="$refs.menu.save(dates)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+      </v-col>
+    </v-row>
+    <v-row class="px-16" no-gutters>
+      <v-col cols="4" class="px-5" v-for="index in 3" :key="index">
+        <span class="pl-5">Obat</span>
+        <v-autocomplete
+              v-model="selectedObat"
+              :items="obat"
+              chips
+              filled
+              multiple
+              clearable
+              deletable-chips
+              rounded
+            >
+        </v-autocomplete>
+      </v-col>
+    </v-row>
+    <v-row class="px-16 pb-10 mt-0">
+      <v-col v-for="apotek in filteredList" :key="apotek" cols="6" class="px-7">
           <v-card elevation="5" class="card">
-            <div class="card-left">
-              <v-card-title class="text-h5 font-weight-bold">
-                {{ apotek.apotek }}
-              </v-card-title>
-
-              <v-card-subtitle>{{ apotek.kota }}, {{ apotek.lokasi }}</v-card-subtitle>
-              <v-card-actions>
-                <v-btn outlined rounded text>
-                  <v-icon class="mr-2">mdi-map-marker</v-icon> Lokasi
-                </v-btn>
-              </v-card-actions>
-            </div>
-            <div class="card-right">
-              <div class="text font-weight-medium">Obat : {{ apotek.obat }}</div>
-              <div class="text">Jumlah: {{ apotek.jumlah }}</div>
-              <div class="text">Updated Date: {{ formatDate(apotek.updatedDate) }}</div>
-              <div class="text">Update Time: {{ apotek.updatedTime }}</div>
-            </div>
+            <v-row class="pa-5">
+              <v-col cols="6">
+                <v-card-title class="text-h5 font-weight-bold pt-0 pl-0">
+                  {{ apotek.apotek }}
+                </v-card-title>
+                <v-card-subtitle class="pa-0">{{ apotek.lokasi }}, {{ apotek.kota }}</v-card-subtitle>
+              </v-col>
+              <v-col cols="6" class="card-right">
+                <div class="text font-weight-bold">{{ apotek.obat }}</div>
+                <div class="text">Sejumlah {{ apotek.jumlah }}</div>
+                <div class="text">Terakhir update {{ formatDate(apotek.updatedDate) }}</div>
+                <div class="text">Jam {{ apotek.updatedTime }}</div>
+                <v-card-actions class="px-0 pt-3">
+                  <v-btn outlined rounded text>
+                    <v-icon class="mr-2">mdi-map-marker</v-icon> Lokasi
+                  </v-btn>
+                </v-card-actions>
+              </v-col>
+            </v-row>
           </v-card>
-        </v-col>
-      </div>
-    </div>
-  </v-container>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -44,20 +113,6 @@
     name: "HelloWorld",
 
     data: () => ({
-      ecosystem: [
-        {
-          text: "vuetify-loader",
-          href: "https://github.com/vuetifyjs/vuetify-loader",
-        },
-        {
-          text: "github",
-          href: "https://github.com/vuetifyjs/vuetify",
-        },
-        {
-          text: "awesome-vuetify",
-          href: "https://github.com/vuetifyjs/awesome-vuetify",
-        },
-      ],
       obat: [
         "Azitromisin",
         "Favipiravir",
@@ -68,6 +123,10 @@
         "Remsdesivir",
         "Tociluzumab",
       ],
+      selectedObat: [],
+      dates: [],
+      menu: false,
+      search: '',
       dummyData: [
         {
           apotek: "Apotek Benedictus",
@@ -107,6 +166,13 @@
         },
       ],
     }),
+    computed: {
+      filteredList() {
+        return this.dummyData.filter(apotek => {
+          return apotek.apotek.toLowerCase().includes(this.search.toLowerCase())
+        })
+      }
+    },
     methods: {
       formatDate(input) {
         var date = new Date(input.substr(4, 4), input.substr(2, 2) - 1, input.substr(0, 2));
@@ -119,6 +185,10 @@
         var result = parseInt(formated[2]) + 1 + "/" + formated[1] + "/" + formated[0];
         return result;
       },
+      remove (item) {
+        const index = this.selectedObat.indexOf(item)
+        if (index >= 0) this.selectedObat.splice(index, 1)
+      }
     },
   };
 </script>
@@ -134,11 +204,9 @@
   }
 
   .card-right {
-    width: 50%;
     display: flex;
     flex-direction: column;
     align-items: flex-end;
-    margin: 1rem 1rem 0 0;
   }
 
   .card-left {
@@ -148,8 +216,8 @@
   .main {
     width: 100vw;
     padding: 5rem 5rem;
-    display: flex;
-    column-gap: 5rem;
+    /* display: flex;
+    column-gap: 5rem; */
   }
 
   .left {
@@ -165,7 +233,6 @@
     max-height: auto;
   }
   .header {
-    height: 30vh;
     width: 100vw;
     background-color: #f9c1b1;
     background-image: linear-gradient(315deg, #f9c1b1 0%, #fb8085 74%);
