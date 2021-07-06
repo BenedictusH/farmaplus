e9d358d7de312032a597548459825a7766642dfa
Kresna Jenie <kresna.jenie@gmail.com>
2021-07-06 15:57:09 +0700

    finished filtered

--- src/api/api.js
+++ src/api/api.js
@@ -26,23 +26,36 @@ export default {
     return axios(config)
       .then(function(response) {
         console.log(JSON.stringify(response.data));
-				var data = {}
-				data['obat'] = Array.from(new Set(response.data.data.outlets.map((outlet) => {
-					return outlet.obat
-				}))).sort()
-				data['provinsi'] = Array.from(new Set(response.data.data.outlets.map((outlet) => {
-					return outlet.provinsi
-				}))).sort()
-				data['kabkota'] = Array.from(new Set(response.data.data.outlets.map((outlet) => {
-					return outlet.kabkota
-				}))).sort()
-				return data
+        var data = {};
+        data["obat"] = Array.from(
+          new Set(
+            response.data.data.outlets.map((outlet) => {
+              return outlet.obat;
+            })
+          )
+        ).sort();
+        data["provinsi"] = Array.from(
+          new Set(
+            response.data.data.outlets.map((outlet) => {
+              return outlet.provinsi;
+            })
+          )
+        ).sort();
+        data["kabkota"] = Array.from(
+          new Set(
+            response.data.data.outlets.map((outlet) => {
+              return outlet.kabkota;
+            })
+          )
+        ).sort();
+        return data;
       })
       .catch(function(error) {
         console.log(error);
       });
   },
   async filter(name, obat, provinsi, kabkota, tanggal) {
+    console.log(name, obat, provinsi, kabkota, tanggal);
     const query = qs.stringify({
       _where: {
         _or: [
@@ -51,16 +64,12 @@ export default {
             { provinsi: provinsi },
             { kabkota: kabkota },
             { tanggal: tanggal },
+						{ nama_contains: name }
           ],
-          [
-            { obat: name },
-            { provinsi: name },
-            { kabkota: name },
-            { tanggal: name },
-          ], // implicit AND
         ],
       },
     });
+    console.log(query);
     var path = "outlets";
     var response = axios.get(`${STRAPI_URL}/${path}?${query}`);
     return response;
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="wrapper">
     <div class="header"></div>
-    {{selected}}
-    <div class="toolbar" style="position: sticky; top: 0; z-index: 20; background-color: white;">
+    {{ selected }}
+    <button @click="filter()">Update</button>
+    <div
+      class="toolbar"
+      style="position: sticky; top: 0; z-index: 20; background-color: white"
+      v-if="!loadingToolbar"
+    >
       <v-row class="px-16 pt-5" no-gutters>
         <v-col cols="8" class="px-5">
           <v-text-field
@@ -11,7 +16,7 @@
             dense
             label="Search"
             append-icon="mdi-magnify"
-            v-model="search"
+            v-model="selected.name"
           ></v-text-field>
         </v-col>
         <v-col cols="4" class="px-5">
@@ -25,22 +30,24 @@
             min-width="auto"
           >
             <template v-slot:activator="{ on, attrs }">
-              <v-combobox
+              <v-text-field
                 v-model="selected.tanggal"
                 filled
                 rounded
                 label="Pilih Tanggal"
-                prepend-inner-icon="mdi-calendar"
+                append-icon="mdi-calendar"
                 v-bind="attrs"
                 v-on="on"
-              ></v-combobox>
+              ></v-text-field>
             </template>
-            <v-date-picker v-model="selected.tanggal" multiple no-title scrollable>
+            <v-date-picker v-model="selected.tanggal" no-title scrollable>
               <v-spacer></v-spacer>
-              <v-btn text color="primary" @click="menu = false">
-                Cancel
-              </v-btn>
-              <v-btn text color="primary" @click="$refs.menu.save(selected.tanggal)">
+              <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.menu.save(selected.tanggal)"
+              >
                 OK
               </v-btn>
             </v-date-picker>
@@ -92,25 +99,38 @@
         </v-col>
       </v-row>
     </div>
-    <v-row class="px-16 pb-10 mt-5">
-      <v-col v-for="apotek in filteredList" :key="apotek" cols="6" class="px-7">
+    <v-row class="px-16 pb-10 mt-5" v-if="!loadingBody">
+      <v-col v-for="apotek in body" :key="apotek" cols="6" class="px-7">
         <v-card elevation="5" class="card">
           <v-row class="pa-5">
             <v-col cols="6">
               <v-card-title class="text-h5 font-weight-bold pt-0 pl-0">
-                {{ apotek.apotek }}
+                {{ apotek.nama }}
               </v-card-title>
-              <v-card-subtitle class="px-0">{{ apotek.lokasi }}, {{ apotek.kota }}</v-card-subtitle>
+              <v-card-subtitle class="px-0"
+                >{{ apotek.kabkota }}, {{ apotek.provinsi }}</v-card-subtitle
+              >
 
-              <v-card-title class="text-h5 font-weight-medium pt-0 pl-0 green--text text--darken-4">
+              <v-card-title
+                class="
+                  text-h5
+                  font-weight-medium
+                  pt-0
+                  pl-0
+                  green--text
+                  text--darken-4
+                "
+              >
                 {{ apotek.jumlah }}
               </v-card-title>
-              <v-card-subtitle class="pa-0 grey--text text--darken-3">Jumlah</v-card-subtitle>
+              <v-card-subtitle class="pa-0 grey--text text--darken-3"
+                >Jumlah</v-card-subtitle
+              >
             </v-col>
             <v-col cols="6" class="card-right">
               <div class="text font-weight-bold">{{ apotek.obat }}</div>
 
-              <div class="text">Didistribusikan pada: {{ formatDate(apotek.updatedDate) }}</div>
+              <div class="text">Didistribusikan pada: {{ apotek.tanggal }}</div>
               <v-card-actions class="px-0 pt-7">
                 <v-btn outlined rounded text>
                   <v-icon class="mr-2">mdi-map-marker</v-icon> Lokasi
@@ -125,219 +145,148 @@
 </template>
 
 <script>
-  import api from '../api/api.js'
+import api from "../api/api.js";
 
+export default {
+  name: "HelloWorld",
 
-  export default {
-    name: "HelloWorld",
-    
-    data: () => ({
-      date: [],
-      menu: false,
-      search: "",
-      selected: {
-        obat: [],
-        provinsi: [],
-        kabkota: [], 
-        tanggal: ""
-      },
-      dummyData: [
-        {
-          apotek: "Apotek Benedictus",
-          obat: "Ivermectin",
-          kota: "Jakarta",
-          lokasi: "Senen",
-          jumlah: 250,
-          updatedDate: "05072021",
-        },
-        {
-          apotek: "Apotek Harris",
-          obat: "Remsdesivir",
-          kota: "Lampung",
-          lokasi: "Lampung",
-          jumlah: 1760,
-          updatedDate: "05072021",
-        },
-        {
-          apotek: "Apotek Hutama",
-          obat: "Immunoglobulin",
-          kota: "Tangerang",
-          lokasi: "Cengkareng",
-          jumlah: 2230,
-          updatedDate: "05072021",
-        },
-        {
-          apotek: "Apotek Stefan",
-          obat: "Oseltamivir",
-          kota: "Yogyakarta",
-          lokasi: "Yogyakarta",
-          jumlah: 10,
-          updatedDate: "05072021",
-        },
-        {
-          apotek: "Apotek Benedictus",
-          obat: "Ivermectin",
-          kota: "Jakarta",
-          lokasi: "Senen",
-          jumlah: 250,
-          updatedDate: "05072021",
-        },
-        {
-          apotek: "Apotek Harris",
-          obat: "Remsdesivir",
-          kota: "Lampung",
-          lokasi: "Lampung",
-          jumlah: 1760,
-          updatedDate: "05072021",
-        },
-        {
-          apotek: "Apotek Hutama",
-          obat: "Immunoglobulin",
-          kota: "Tangerang",
-          lokasi: "Cengkareng",
-          jumlah: 2230,
-          updatedDate: "05072021",
-        },
-        {
-          apotek: "Apotek Stefan",
-          obat: "Oseltamivir",
-          kota: "Yogyakarta",
-          lokasi: "Yogyakarta",
-          jumlah: 10,
-          updatedDate: "05072021",
-        },
-      ],
-      options: {
-        obat: [
-          "Azitromisin",
-          "Favipiravir",
-          "Intravenous",
-          "Immunoglobulin",
-          "Ivermectin",
-          "Oseltamivir",
-          "Remsdesivir",
-          "Tociluzumab",
-        ],
-          provinsi: [
-          "DKI Jakarta",
-          "Jawa Barat",
-          "Jawa Timur",
-          "Jawa Tengah",
-          "Bali",
-          "Kalimantan Utara",
-          "Kalimantan Selatan",
-          "Kalimantan Barat",
-        ],
-          kabkota: [
-          "Jakarta Barat",
-          "Bekasi",
-          "Tangerang",
-          "Bandar Lampung",
-          "Denpasar",
-          "Jambi",
-          "Singkawang",
-          "Banjarmasin",
-        ],
-      }
-    }),
-    
-    computed: {
-      filteredList() {
-        return this.dummyData.filter((apotek) => {
-          return apotek.apotek.toLowerCase().includes(this.search.toLowerCase());
-        });
-      },
+  data: () => ({
+    date: [],
+    menu: false,
+    loadingToolbar: true,
+    loadingBody: true,
+    selected: {
+      name: [],
+      obat: [],
+      provinsi: [],
+      kabkota: [],
+      tanggal: [],
     },
-    methods: {
-      formatDate(input) {
-        var date = new Date(input.substr(4, 4), input.substr(2, 2) - 1, input.substr(0, 2));
-        var formated = date
-          .toISOString()
-          .replace("-", "/")
-          .split("T")[0]
-          .replace("-", "/")
-          .split("/");
-        var result = parseInt(formated[2]) + 1 + "/" + formated[1] + "/" + formated[0];
-        return result;
-      },
-      remove(item) {
-        const index = this.selectedObat.indexOf(item);
-        if (index >= 0) this.selectedObat.splice(index, 1);
-      },
-      async refreshPage() {
-        console.log(await api.filter())
-        console.log(await api.getField())
-      }
+    body: [],
+    options: [],
+  }),
+  methods: {
+    formatDate(input) {
+      var date = new Date(
+        input.substr(4, 4),
+        input.substr(2, 2) - 1,
+        input.substr(0, 2)
+      );
+      var formated = date
+        .toISOString()
+        .replace("-", "/")
+        .split("T")[0]
+        .replace("-", "/")
+        .split("/");
+      var result =
+        parseInt(formated[2]) + 1 + "/" + formated[1] + "/" + formated[0];
+      return result;
     },
-    created() {
-      this.refreshPage()
+    remove(item) {
+      const index = this.selectedObat.indexOf(item);
+      if (index >= 0) this.selectedObat.splice(index, 1);
+    },
+    async refreshPage() {
+      this.options = await api.getField();
+      this.loadingToolbar = false;
+
+      var res = await api.filter();
+      this.body = res.data;
+      this.loadingBody = false;
+    },
+    async update() {
+      this.loadingBody = true;
+
+      var res = await api.filter(
+        this.selected.name,
+        this.selected.obat,
+        this.selected.provinsi,
+        this.selected.kabkota,
+        this.selected.tanggal
+      );
+      this.body = res.data;
+      console.log(res.data);
+      this.loadingBody = false;
+    },
+    reset() {
+      this.selected = {
+      name: [],
+      obat: [],
+      provinsi: [],
+      kabkota: [],
+      tanggal: [],
     }
-  };
+    }
+  },
+  async created() {
+    await this.refreshPage();
+  },
+};
 </script>
 
 <style scoped>
-  .wrapper {
-    padding: 0;
-    margin: 0;
-  }
+.wrapper {
+  padding: 0;
+  margin: 0;
+}
 
-  .card {
-    display: flex;
-  }
+.card {
+  display: flex;
+}
 
-  .card-right {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-  }
+.card-right {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
 
-  .card-left {
-    width: 50%;
-  }
+.card-left {
+  width: 50%;
+}
 
-  .toolbar {
-    box-shadow: 0px 5px 5px 0px rgba(255, 255, 255, 0.85);
-    -webkit-box-shadow: 0px 5px 5px 0px rgba(255, 255, 255, 0.85);
-    -moz-box-shadow: 0px 5px 5px 0px rgba(255, 255, 255, 0.85);
-  }
+.toolbar {
+  box-shadow: 0px 5px 5px 0px rgba(255, 255, 255, 0.85);
+  -webkit-box-shadow: 0px 5px 5px 0px rgba(255, 255, 255, 0.85);
+  -moz-box-shadow: 0px 5px 5px 0px rgba(255, 255, 255, 0.85);
+}
 
-  .main {
-    width: 100vw;
-    padding: 5rem 5rem;
-    /* display: flex;
+.main {
+  width: 100vw;
+  padding: 5rem 5rem;
+  /* display: flex;
     column-gap: 5rem; */
-  }
+}
 
-  .left {
-    width: 30%;
-  }
+.left {
+  width: 30%;
+}
 
-  .right {
-    width: 70%;
-  }
+.right {
+  width: 70%;
+}
 
-  .card {
-    min-height: 10rem;
-    max-height: auto;
-  }
-  .header {
-    width: 100vw;
-    height: 300px;
-    background-color: #90d5ec;
-    background-image: linear-gradient(315deg, #90d5ec 0%, #fc575e 74%);
-    background-size: 400% 400%;
-    animation: gradient 10s ease infinite;
-  }
+.card {
+  min-height: 10rem;
+  max-height: auto;
+}
+.header {
+  width: 100vw;
+  height: 300px;
+  background-color: #90d5ec;
+  background-image: linear-gradient(315deg, #90d5ec 0%, #fc575e 74%);
+  background-size: 400% 400%;
+  animation: gradient 10s ease infinite;
+}
 
-  @keyframes gradient {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
   }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
 </style>
