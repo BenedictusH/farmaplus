2c2d2ec738098de9a8f6bbd306ef289a1bce62dd
stefan19052003 <stefan19052003@gmail.com>
2021-07-11 18:56:34 +0700

    fix meta in index html

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title= 'Farmaplus Kementerian Kesehatan'
+        return args
+      })
+  },
   transpileDependencies: [
     'vuetify'
   ]
