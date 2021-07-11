d453f8d91e07063cbb6a936f45610f03eca13148
stefan19052003 <stefan19052003@gmail.com>
2021-07-11 13:56:09 +0700

    add disclaimer

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -89,11 +89,11 @@
               />
             </v-col>
           </v-row>
-            <div class="text-lg-subtitle-2 text-caption py-5" cols="12" md="6" order-md="1" order="3" v-if="!loadingToolbar" >
-              <strong> 
-                *Saat ini, data diupdate secara berkala tiap hari pkl 09.00 WIB, sehingga bukan data realtime. Sehubungan dengan tingginya kebutuhan, harap konfirmasikan ketersediaan di masing-masing lokasi apotek.
-              </strong>
-            </div>
+          <div class="text-lg-subtitle-2 text-caption py-5" cols="12" md="6" order-md="1" order="3" v-if="!loadingToolbar" >
+            <strong> 
+              *Saat ini, data diupdate secara berkala tiap hari pkl 09.00 WIB, sehingga bukan data realtime. Sehubungan dengan tingginya kebutuhan, harap konfirmasikan ketersediaan di masing-masing lokasi apotek.
+            </strong>
+          </div>
 
           <!-- {{ formatTanggal(apotek.tanggal) }} -->
           <v-data-table
