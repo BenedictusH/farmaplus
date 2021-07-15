481125ddc19e6bf0ce5ff41c8e04e9cea391dd0f
Kresna Jenie <kresna.jenie@gmail.com>
2021-07-15 23:28:27 +0700

    added obatDetail

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -95,7 +95,7 @@
                 src="https://upload.wikimedia.org/wikipedia/id/thumb/c/c3/Kimia_Farma_logo.svg/1200px-Kimia_Farma_logo.svg.png"
                 class="logo"
               />
-              <img src="https://i.ibb.co/3pPTRBk/berkat.jpg" class="logo" />
+              <img src="https://i.ibb.co/fDRd2Vj/message-Image-1626365949727.jpg" class="logo" />
               <!-- <img
                 src="https://logos-download.com/wp-content/uploads/2017/01/Watsons_logo_logotype.png"
                 class="logo"
@@ -238,12 +238,12 @@
         class="px-7"
         v-show="!loadingBody"
       >
-        <v-card elevation="5" class="card d-flex flex-column" height="100%">
+        <v-card elevation="5" class="card d-flex flex-column" height="100%" style="border-radius:15px;">
           <v-row class="pa-5">
             <v-col cols="12" lg="7" class="d-flex flex-column justify-space-between">
               <div>
                 <img :src="logoBrand(apotek.brand)" alt="Logo Apotek" class="logo-card mb-3" />
-                <v-card-title class="text-lg-h5 text-md-h4 font-weight-bold pt-0 pl-0">
+                <v-card-title class="text-lg-h5 text-md-h4 font-weight-bold pt-0 pl-0" style="word-wrap: normal;">
                   {{ formatNama(apotek.nama) }}
                 </v-card-title>
                 <v-card-subtitle class="px-0 pb-0">{{ apotek.alamat }}</v-card-subtitle
@@ -276,12 +276,15 @@
             >
               <div>
                 <v-card-title
-                  class="text font-weight-bold px-0 d-flex justify-start justify-lg-end"
+                  class="text-right font-weight-bold px-0 d-flex flex-column justify-start justify-lg-end align-lg-end align-start"
                 >
-                  <span>{{ apotek.obat }} </span></v-card-title
+                  <span>{{ apotek.obat }} </span>
+                  <span class="font-weight-regular text-caption">{{ apotek.obatDetail }} </span>
+                  </v-card-title
                 >
+                
                 <v-card-subtitle
-                  class="pa-0 grey--text text-h6 text--darken-3 text-lg-right pt-lg-10 pt-5"
+                  class="pa-0 grey--text text-h6 text--darken-3 text-lg-right pt-5"
                   >Stok</v-card-subtitle
                 >
                 <v-card-title
@@ -294,6 +297,9 @@
                     pb-2
                     green--text
                     text--darken-4
+                    d-flex
+                    justify-lg-end
+                    justify-start
                   "
                 >
                   <span v-if="apotek.status == 'habis'" class="red--text spaccing">
@@ -322,7 +328,7 @@
                   v-if="apotek.wa"
                 >
                   <a
-                    class="d-flex align-center justify-center whatsapp"
+                    class="d-flex align-center justify-center whatsapp pt-0"
                     v-bind:href="getWa(apotek)"
                     target="_blank"
                   >
@@ -342,7 +348,7 @@
                     </v-btn>
                   </a>
                 </div>
-                <div class="pt-md-0 pt-3">
+                <div class="pt-md-0 pt-3 pb-0">
                   <a
                     :href="'tel:' + formatTelpon(apotek.msisdn)"
                     v-if="checkNull(apotek.msisdn)"
@@ -376,7 +382,30 @@
           <!-- card-actions for md and up -->
           <v-row class="flex-grow-1 pa-8 pt-0 align-end d-none d-md-flex">
             <v-col cols="12" class="pa-0 ">
-              <v-card-actions class="pa-0 d-flex flex-row align-center  justify-end">
+              <v-card-actions class="pa-0 d-flex flex-row align-center  justify-start ">
+                <div>
+                  <a v-bind:href="getLoc(apotek.latitude, apotek.longitude)" target="_blank">
+                    <v-btn rounded color="success mr-md-2">
+                      <v-icon class="mr-md-2">mdi-map-marker</v-icon>
+                      <span class="d-none d-md-block">Maps</span>
+                    </v-btn>
+                  </a>
+                </div>
+                
+                <div>
+                  <a :href="'tel:' + formatTelpon(apotek.msisdn)" v-if="checkNull(apotek.msisdn)">
+                    <v-btn rounded color="info" class="mr-md-2">
+                      <v-icon class="mr-md-2">mdi-phone</v-icon>
+                      <span class="d-none d-md-block">{{ formatTelpon(apotek.msisdn) }}</span>
+                    </v-btn>
+                  </a>
+                  <a v-else class>
+                    <v-btn rounded outlined disabled color="info" class="mr-md-2">
+                      <v-icon class="mr-md-2">mdi-phone</v-icon>
+                      <span class="d-none d-md-block">Tidak Tersedia</span>
+                    </v-btn>
+                  </a>
+                </div>
                 <div
                   class="d-flex align-center justify-center whatsapp"
                   v-if="apotek.wa"
@@ -401,28 +430,22 @@
                       </svg>
                     </v-btn>
                   </a>
-                </div>
-                <div>
-                  <a :href="'tel:' + formatTelpon(apotek.msisdn)" v-if="checkNull(apotek.msisdn)">
-                    <v-btn rounded color="info" class="mr-md-2">
-                      <v-icon class="mr-md-2">mdi-phone</v-icon>
-                      <span class="d-none d-md-block">{{ formatTelpon(apotek.msisdn) }}</span>
+                </div>  
+                <div v-else>
+                  <v-btn rounded outlined disabled color="info" class="mr-md-2">
+                      <svg
+                        class="filterGrey"
+                        height="25"
+                        viewBox="-23 -21 682 682.66669"
+                        width="25"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="m544.386719 93.007812c-59.875-59.945312-139.503907-92.9726558-224.335938-93.007812-174.804687 0-317.070312 142.261719-317.140625 317.113281-.023437 55.894531 14.578125 110.457031 42.332032 158.550781l-44.992188 164.335938 168.121094-44.101562c46.324218 25.269531 98.476562 38.585937 151.550781 38.601562h.132813c174.785156 0 317.066406-142.273438 317.132812-317.132812.035156-84.742188-32.921875-164.417969-92.800781-224.359376zm-224.335938 487.933594h-.109375c-47.296875-.019531-93.683594-12.730468-134.160156-36.742187l-9.621094-5.714844-99.765625 26.171875 26.628907-97.269531-6.269532-9.972657c-26.386718-41.96875-40.320312-90.476562-40.296875-140.28125.054688-145.332031 118.304688-263.570312 263.699219-263.570312 70.40625.023438 136.589844 27.476562 186.355469 77.300781s77.15625 116.050781 77.132812 186.484375c-.0625 145.34375-118.304687 263.59375-263.59375 263.59375zm144.585938-197.417968c-7.921875-3.96875-46.882813-23.132813-54.148438-25.78125-7.257812-2.644532-12.546875-3.960938-17.824219 3.96875-5.285156 7.929687-20.46875 25.78125-25.09375 31.066406-4.625 5.289062-9.242187 5.953125-17.167968 1.984375-7.925782-3.964844-33.457032-12.335938-63.726563-39.332031-23.554687-21.011719-39.457031-46.960938-44.082031-54.890626-4.617188-7.9375-.039062-11.8125 3.476562-16.171874 8.578126-10.652344 17.167969-21.820313 19.808594-27.105469 2.644532-5.289063 1.320313-9.917969-.664062-13.882813-1.976563-3.964844-17.824219-42.96875-24.425782-58.839844-6.4375-15.445312-12.964843-13.359374-17.832031-13.601562-4.617187-.230469-9.902343-.277344-15.1875-.277344-5.28125 0-13.867187 1.980469-21.132812 9.917969-7.261719 7.933594-27.730469 27.101563-27.730469 66.105469s28.394531 76.683594 32.355469 81.972656c3.960937 5.289062 55.878906 85.328125 135.367187 119.648438 18.90625 8.171874 33.664063 13.042968 45.175782 16.695312 18.984374 6.03125 36.253906 5.179688 49.910156 3.140625 15.226562-2.277344 46.878906-19.171875 53.488281-37.679687 6.601563-18.511719 6.601563-34.375 4.617187-37.683594-1.976562-3.304688-7.261718-5.285156-15.183593-9.253906zm0 0"
+                          fill-rule="evenodd"
+                        />
+                      </svg>
                     </v-btn>
-                  </a>
-                  <a v-else class>
-                    <v-btn rounded outlined disabled color="info" class="mr-md-2">
-                      <v-icon class="mr-md-2">mdi-phone</v-icon>
-                      <span class="d-none d-md-block">Tidak Tersedia</span>
-                    </v-btn>
-                  </a>
-                </div>
-                <div>
-                  <a v-bind:href="getLoc(apotek.latitude, apotek.longitude)" target="_blank">
-                    <v-btn rounded color="success">
-                      <v-icon class="mr-md-2">mdi-map-marker</v-icon>
-                      <span class="d-none d-md-block">Maps</span>
-                    </v-btn>
-                  </a>
                 </div>
               </v-card-actions>
             </v-col>
@@ -642,7 +665,7 @@
         if (merek == "KF") {
           return "https://upload.wikimedia.org/wikipedia/id/thumb/c/c3/Kimia_Farma_logo.svg/1200px-Kimia_Farma_logo.svg.png";
         } else if (merek == "BERKAT") {
-          return "https://i.ibb.co/3pPTRBk/berkat.jpg";
+          return "https://i.ibb.co/fDRd2Vj/message-Image-1626365949727.jpg";
         } else if (merek == "WATSONS") {
           return "https://logos-download.com/wp-content/uploads/2017/01/Watsons_logo_logotype.png";
         } else if (merek == "K24") {
@@ -960,7 +983,7 @@
   .logo {
     height: 50px;
     object-fit: contain;
-    max-width: 100px;
+    max-width: 150px;
   }
 
   .wrapper {
@@ -1024,7 +1047,6 @@
   }
 
   .whatsapp {
-    height: 100%;
   }
 
   .filterGreen {
@@ -1034,6 +1056,10 @@
 
   .filterGreen:hover {
     filter: invert(90%) sepia(96%) saturate(421%) hue-rotate(80deg) brightness(95%) contrast(86%);
+  }
+
+  .filterGrey {
+    filter: invert(70%);
   }
 
   .badgeDistributor {
