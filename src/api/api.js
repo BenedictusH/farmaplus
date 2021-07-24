8a6ed8bc46312925e41c4552c7974ed2d918aae1
Vannes Wijaya <vannesoey@gmail.com>
2021-07-24 21:10:23 +0700

    api

--- src/api/api.js
+++ src/api/api.js
@@ -25,7 +25,7 @@ export default {
   //   };
   //   return axios(config)
   //     .then(function(response) {
-  //       // 
+  //       //
   //       var data = {};
   //       data["obat"] = Array.from(
   //         new Set(
@@ -51,11 +51,10 @@ export default {
   //       return data;
   //     })
   //     .catch(function(error) {
-  //       
+  //
   //     });
   // },
   async filter(name, obat, provinsi, kabkota, brand, tanggal, start, limit) {
-    
     const query = qs.stringify({
       _where: {
         _or: [
@@ -65,19 +64,21 @@ export default {
             { kabkota_contains: kabkota },
             { brand_contains: brand },
             { tanggal_contains: tanggal },
-            { nama_contains: name }
+            { nama_contains: name },
           ],
         ],
       },
     });
-    // 
+    //
     var path = "outlets";
-    var newStart = start * limit
-    var response = axios.get(`${STRAPI_URL}/${path}?${query}&_sort=jumlah:DESC&_start=${newStart}&_limit=${limit}`);
+    var newStart = start * limit;
+    var response = axios.get(
+      `${STRAPI_URL}/${path}?${query}&_sort=jumlah:DESC&_start=${newStart}&_limit=${limit}`
+    );
     return response;
   },
   async count(name, obat, provinsi, kabkota, brand, tanggal) {
-    // 
+    //
     const query = qs.stringify({
       _where: {
         _or: [
@@ -87,35 +88,95 @@ export default {
             { kabkota_contains: kabkota },
             { brand_contains: brand },
             { tanggal_contains: tanggal },
-            { nama_contains: name }
+            { nama_contains: name },
           ],
         ],
       },
     });
-    // 
+    //
     var path = "outlets";
-    var response = axios.get(`${STRAPI_URL}/${path}/count?${query}&_sort=jumlah:DESC`);
-    // 
+    var response = axios.get(
+      `${STRAPI_URL}/${path}/count?${query}&_sort=jumlah:DESC`
+    );
+    //
     return response;
   },
-  async getLastShipment () {
+  async getLastShipment() {
     var path = "outlets";
-    var response = await axios.get(`${STRAPI_URL}/${path}?&_sort=tanggal:desc&_start=0&_limit=1`);
-    
+    var response = await axios.get(
+      `${STRAPI_URL}/${path}?&_sort=tanggal:desc&_start=0&_limit=1`
+    );
+
     return response;
   },
   async find(name) {
-    
     var path = name;
-    
+
     var response = await axios.get(`${STRAPI_URL}/${path}?&_sort=nama:ASC`);
-    
+
     return response.data;
   },
   async filterAPI(name, start, limit) {
     var path = name;
-    var newStart = start * limit
-    var response = axios.get(`${STRAPI_URL}/${path}?&_sort=jumlah:DESC&_start=${newStart}&_limit=${limit}`);
+    var newStart = start * limit;
+    var response = axios.get(
+      `${STRAPI_URL}/${path}?&_sort=jumlah:DESC&_start=${newStart}&_limit=${limit}`
+    );
+    return response;
+  },
+  async filterCollection(
+    path,
+    name,
+    obat,
+    provinsi,
+    kabkota,
+    brand,
+    tanggal,
+    start,
+    limit
+  ) {
+    const query = qs.stringify({
+      _where: {
+        _or: [
+          [
+            { obat_contains: obat },
+            { provinsi_contains: provinsi },
+            { kabkota_contains: kabkota },
+            { brand_contains: brand },
+            { tanggal_contains: tanggal },
+            { nama_contains: name },
+          ],
+        ],
+      },
+    });
+    //
+    var newStart = start * limit;
+    var response = axios.get(
+      `${STRAPI_URL}/${path}?${query}&_sort=jumlah:DESC&_start=${newStart}&_limit=${limit}`
+    );
+    return response;
+  },
+  async countCollection(path, name, obat, provinsi, kabkota, brand, tanggal) {
+    //
+    const query = qs.stringify({
+      _where: {
+        _or: [
+          [
+            { obat_contains: obat },
+            { provinsi_contains: provinsi },
+            { kabkota_contains: kabkota },
+            { brand_contains: brand },
+            { tanggal_contains: tanggal },
+            { nama_contains: name },
+          ],
+        ],
+      },
+    });
+    //
+    var response = axios.get(
+      `${STRAPI_URL}/${path}/count?${query}&_sort=jumlah:DESC`
+    );
+    //
     return response;
   },
 };
